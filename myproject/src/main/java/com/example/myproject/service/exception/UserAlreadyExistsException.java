2a65968a95f448847bc505efa3bc82620fa4c24f
Alexander Lipatov <alexlpt@yandex.ru>
2015-07-30 02:16:02 +0300

    Get rid of unused class

--- myproject/src/main/java/com/example/myproject/service/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.service.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(final String message) {
-        super(message);
-    }
-}
